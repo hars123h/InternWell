8a3cc4daa103934aa5e8e397928c0a4b596a93a8
Aman <amanchopra.atg@gmail.com>
2021-08-31 22:11:17 +0530

    added developers photos

--- client/src/pages/Team.js
+++ client/src/pages/Team.js
@@ -45,8 +45,8 @@ const Team = () => {
 										<img src="../images/t3.jpg" className="img-responsive" alt="#" />
 										<div className="social">
 											<ul className="list-inline">
-												<li><a href="#"><i className="fa fa-linkedin"></i></a></li>
-												<li><a href="#"><i className="fa fa-instagram"></i></a></li>
+												<li><a href="https://www.linkedin.com/in/sachinkorla"><i className="fa fa-linkedin"></i></a></li>
+												<li><a href="https://instagram.com/loverespect_sachu"><i className="fa fa-instagram"></i></a></li>
 											</ul>
 										</div>
 									</div>
@@ -63,13 +63,13 @@ const Team = () => {
 								<div className="single-team">
 									{/* <!-- Team Head --> */}
 									<div className="img-area">
-										<img src="../images/t4.jpg" className="img-responsive" alt="#" />
+										<img src="../images/m40.jpeg" className="img-responsive" alt="#" />
 										<div className="social">
 											<ul className="list-inline">
-												<li><a href="#">
+												<li><a href="https://www.linkedin.com/in/ayush-thakur-282000130/">
 													<i className="fa fa-linkedin"></i>
 												</a></li>
-												<li><a href="#">
+												<li><a href="https://www.instagram.com/ayush83461/">
 													<i className="fa fa-instagram"></i>
 												</a></li>
 											</ul>
@@ -89,10 +89,113 @@ const Team = () => {
 						</div>
 					</div>
 				</div>
-
-
 			</section>
 			{/* <!--/ End Team --> */}
+
+			{/* developers team starting */}
+			<section id="team" className="team section">
+				<div className="container">
+					<div className="row">
+						<div className="col-12">
+							<div className="section-title">
+								<h1>Our Developers</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="team-area">
+					<div className="container">
+						<div className="row">
+							<div className=" col-lg-4 col-10  mx-auto ">
+								{/* <!-- Single Team --> */}
+								<div className="single-team">
+									{/* <!-- Team Head --> */}
+									<div className="img-area">
+										<img src="../images/aman.jpg" className="img-responsive" alt="#" />
+										<div className="social">
+											<ul className="list-inline">
+												<li><a href="https://www.linkedin.com/in/aman-chopra-375a81171/"><i className="fa fa-linkedin"></i></a></li>
+												<li><a href="https://www.github.com/Aman1905"><i className="fa fa-github"></i></a></li>
+												<li><a href="https://twitter.com/iam_chopra_aman"><i className="fa fa-twitter"></i></a></li>
+											</ul>
+										</div>
+									</div>
+									<div className="img-text">
+										<h4>Aman Chopra</h4>
+										<p>MERN Developer</p>
+									</div>
+								</div>
+							</div>
+
+
+							<div className=" col-lg-4 col-10  mx-auto ">
+								{/* <!-- Single Team --> */}
+								<div className="single-team">
+									{/* <!-- Team Head --> */}
+									<div className="img-area">
+										<img src="../images/harsh.jpeg" className="img-responsive" alt="#" />
+										<div className="social">
+											<ul className="list-inline">
+												<li><a href="https://www.linkedin.com/in/tripathi-harsh/">
+													<i className="fa fa-linkedin"></i>
+												</a></li>
+												<li><a href="https://github.com/hars123h">
+													<i className="fa fa-github"></i>
+												</a></li>
+												<li><a href="https://www.upwork.com/freelancers/~0125e41c3bf16e2b53?viewMode=1">
+													<i className="fa fa-briefcase"></i>
+												</a></li>
+											</ul>
+										</div>
+									</div>
+									<div className="img-text">
+										<h4>Harsh Tripathi</h4>
+										<p>MERN Developer</p>
+									</div>
+									{/* <!-- Team Bottom --> */}
+
+									{/* <!--/ End Team Bottom --> */}
+								</div>
+								{/* <!-- End Single Team --> */}
+							</div>
+
+							<div className=" col-lg-4 col-10  mx-auto ">
+								{/* <!-- Single Team --> */}
+								<div className="single-team">
+									{/* <!-- Team Head --> */}
+									<div className="img-area">
+										<img src="../images/vaibhav.jpeg" className="img-responsive" alt="#" />
+										<div className="social">
+											<ul className="list-inline">
+												<li><a href=" https://www.linkedin.com/in/vaibhav-bhardwaj2000">
+													<i className="fa fa-linkedin"></i>
+												</a></li>
+												<li><a href="https://www.instagram.com/__vaibhav.bhardwaj__/">
+													<i className="fa fa-instagram"></i>
+												</a></li>
+												<li><a href="https://twitter.com/Vaibhav_2000_?s=08">
+													<i className="fa fa-twitter"></i>
+												</a></li>
+											</ul>
+										</div>
+									</div>
+									<div className="img-text">
+										<h4>Vaibhav Bhardwaj</h4>
+										<p>Frontend Developer</p>
+									</div>
+									{/* <!-- Team Bottom --> */}
+
+									{/* <!--/ End Team Bottom --> */}
+								</div>
+								{/* <!-- End Single Team --> */}
+							</div>
+
+
+						</div>
+					</div>
+				</div>
+			</section>
+			{/* developers team ending */}
 		</>
 	)
 }
