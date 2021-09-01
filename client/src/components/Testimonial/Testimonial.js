0ca95b5541f780d50c8fb2ca1a81f3f6dae321bc
HARSH TRIPATHI <tripathihk2014@gmail.com>
2021-09-01 10:48:55 +0530

    Update Testimonial.js
    Bug fixed on testimonial

--- client/src/components/Testimonial/Testimonial.js
+++ client/src/components/Testimonial/Testimonial.js
@@ -8,56 +8,65 @@ const PreviousBtn = (props) => {
     console.log(props);
     const { className, onClick } = props;
     return (
-      <div className={className} onClick={onClick}>
-      <i className="fa fa-chevron-left" style={{ color: "gray", fontSize: "30px" }} aria-hidden="true"></i>
-      </div>
+        <div className={className} onClick={onClick}>
+            <i className="fa fa-chevron-left" style={{ color: "gray", fontSize: "30px" }} aria-hidden="true"></i>
+        </div>
     );
-  };
-  const NextBtn = (props) => {
+};
+const NextBtn = (props) => {
     const { className, onClick } = props;
     return (
-      <div className={className} onClick={onClick}>
-      <i className="fa fa-chevron-right" aria-hidden="true" style={{ color: "gray", fontSize: "30px" }}></i>
-      </div>
+        <div className={className} onClick={onClick}>
+            <i className="fa fa-chevron-right" aria-hidden="true" style={{ color: "gray", fontSize: "30px" }}></i>
+        </div>
     );
-  };
+};
 
 const Testimonial = () => {
-    
+
     return (
         <div className="outermost testimonial">
-           <div style={{width: "50%"}}>
-            <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
-                <Card
-                    img="./images/m40.jpeg"
-                    alt="Testinomial Image 1"
+            <div style={{ width: "50%" }}>
+                <Slider prevArrow={<PreviousBtn />} nextArrow={<NextBtn />} dots>
+                    <Card
+                        img="./images/m40.jpeg"
+                        alt="Testinomial Image 1"
+                        clientDescription="Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Dolorum dignissimos dolor eum fugiat vero nobis doloribus
+                    impedit blanditiis recusandae, quibusdam rerum hic
+                    , perferendis obcaecati laudantium harum eius quia dolore debitis."
+                    clientName="Harsh Tripathi"
+                    clientPosition="Developer"
                 />
-                <Card
-                    img="./images/t3.jpg"
-                    alt="Testinomial Image 1"
-                />
-            </Slider>
+                    <Card
+                        img="./images/t3.jpg"
+                        alt="Testinomial Image 1"
+                        clientDescription="Lorem ipsum dolor, sit amet
+                    consectetur adipisicing elit. Dolorum dignissimos dolor eum fugiat vero nobis doloribus
+                    impedit blanditiis recusandae, quibusdam rerum hic
+                    , perferendis obcaecati laudantium harum eius quia dolore debitis."
+                    clientName="Jon Doe"
+                    clientPosition="Designer"
+
+                    />
+                </Slider>
+            </div>
         </div>
-        </div>
-        
+
     )
 }
 
-const Card = ({ img, alt }) => {
+const Card = ({ img, alt, clientDescription, clientName, clientPosition }) => {
     return (
         <div className="image_style ">
             <img src={img} alt={alt} className="avatar" />
-            <p style={{color: 'gray'}}>Lorem, ipsum dolor sit amet consectetur adipisicing
-             elit. Animi veniam excepturi quos consequatur distinctio
-              quaerat aliquid id in natus ut, ex, qui facere ipsum
-               harum dolorum quia quo repudiandae facilis? Quod,
-                tenetur fugiat! Molestias 
-            minus consequatur, culpa laborum doloremque sapiente.
+            <p style={{ color: 'gray' }}>{clientDescription}
             </p>
-            <p style={{fontStyle: 'italic',
-             marginTop:"25px"
+            <p style={{
+                fontStyle: 'italic',
+                marginTop: "25px"
             }}>
-            <span className="t_name">Harsh Tripathi</span> , Developer
+                <span className="t_name">{clientName}</span> , {clientPosition}
             </p>
         </div>
     )
