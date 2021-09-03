5814947a1d28115335ce477fe9a03b31468b8883
HARSH TRIPATHI <tripathihk2014@gmail.com>
2021-09-03 23:52:57 +0530

    Modiying the team Section

--- client/src/pages/NewTeam.js
+++ client/src/pages/NewTeam.js
@@ -60,20 +60,23 @@ const NewTeam = () => {
                     <div class="col-12 col-sm-6 col-md-4 col-lg-4">
                         <div class="our-team">
                             <div class="picture">
-                                <img class="img-fluid" src="https://picsum.photos/130/130?image=836" />
+                                <img class="img-fluid" src="https://thumbs.dreamstime.com/b/web-development-vector-icon-symbol-creative-sign-seo-icons-collection-filled-flat-illustration-computer-mobile-logo-155906362.jpg" />
                             </div>
                             <div class="team-content">
                                 <h3 class="name">Mary Huntley</h3>
-                                <h4 class="title">Web Developer</h4>
+                                
+                                <p style={{padding: '15px'}}>In the current scenario, business and startus culture are growing rapidly especially in India and thats why we have bring this consulting service in
+                                 which we will help people with their business development and
+                                  other things related to grow their market value.
+                                </p>
                             </div>
-                            <ul class="social">
-                                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-facebook" aria-hidden="true"></a></li>
-                                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-twitter" aria-hidden="true"></a></li>
-                                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-google-plus" aria-hidden="true"></a></li>
-                                <li><a href="https://codepen.io/collection/XdWJOQ/" class="fa fa-linkedin" aria-hidden="true"></a></li>
-                            </ul>
+                            
                         </div>
+
+
+
                     </div>
+                    <h3>Last One is for Service Modify it </h3>
                 </div>
             </div>
         </>
