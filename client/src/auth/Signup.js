7e94d33345a23310f0d79e34bcd9d5e856401175
Aman <amanchopra.atg@gmail.com>
2021-09-14 00:45:37 +0530

    completed signup and signin page

--- client/src/auth/Signup.js
+++ client/src/auth/Signup.js
@@ -4,13 +4,14 @@ import axios from 'axios';
 import { isAuth } from './helpers';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
+import './Login.css'
 
 const Signup = () => {
     const [values, setValues] = useState({
         name: '',
         email: '',
         password: '',
-        buttonText: 'Submit'
+        buttonText: 'Create Account'
     });
 
     const { name, email, password, buttonText } = values;
@@ -41,43 +42,62 @@ const Signup = () => {
     };
 
     const signupForm = () => (
-        <form>
+        <form className="myForm text-center">
+            <header>Create New Account</header>
+
                 <div className="form-group">
-                    <label className="text-muted">Name</label>
-                    <input onChange={handleChange('name')} value={name} type="text" className="form-control" />
+                    <i className= "fa fa-user"></i>
+                    <input onChange={handleChange('name')} value={name} className="myInput" type="text" placeholder="Username" />
                 </div>
 
                 <div className="form-group">
-                    <label className="text-muted">Email</label>
-                    <input onChange={handleChange('email')} value={email} type="email" className="form-control" />
+                    <i className= "fa fa-envelope"></i>
+                    <input onChange={handleChange('email')} value={email} className="myInput" type="email" placeholder="Email" />
                 </div>
 
                 <div className="form-group">
-                    <label className="text-muted">Password</label>
-                    <input onChange={handleChange('password')} value={password} type="password" className="form-control" />
+                    <i className= "fa fa-lock"></i>
+                    <input onChange={handleChange('password')} value={password} className="myInput" type="password" placeholder="Password" />
                 </div>
 
+                {/* <input type="submit" className="butt" value="CREATE ACCOUNT" /> */}
                 <div className="form-group button">
-                    <button className="btn btn-primary py-2" onClick={clickSubmit}>
+                    <button className="butt" onClick={clickSubmit}>
                         {buttonText}
                     </button>
                 </div>
+                <Link to = "/signin">
+                    <p style={{color: "#005AC1"}}>Already have an account ? </p>
+                </Link>
         </form>
     );
 
     return (
         <>
-        <div className="col-md-6 offset-md-3">
-            <ToastContainer /> 
-            {isAuth() ? <Redirect to="/" /> : null}
-                <h1 className="p-5 text-center">Signup</h1>
-            {signupForm()}
-            <br />
-            <Link to = "/signin">
-                <h6>Already have an account ? </h6>
-            </Link>
-            <br /> <br /> <br /> <br /> <br /> <br />
+        <div className="h">
+            <div className="container">
+                <div className="myCard">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="myLeftCtn">
+                                <ToastContainer /> 
+                                {isAuth() ? <Redirect to="/" /> : null}
+                                {signupForm()}
+                                <br />
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                        <div className="myRightCtn">
+                            <div className="box">
+                                <img src='./images/signup.png' alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
         </>
     );
 };
